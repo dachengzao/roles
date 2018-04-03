70c3fb6206a1b12692d4b1a3ae9eeeebaefdba94
dachengzao <2383071670@qq.com>
2018-04-03 22:07:09 +0800

    权限接口

--- permissioner.go
+++ permissioner.go
@@ -1,10 +1,12 @@
 package roles
 
+// 定义权限接口
 // Permissioner permissioner interface
 type Permissioner interface {
 	HasPermission(mode PermissionMode, roles ...interface{}) bool
 }
 
+// 合并权限数据
 // ConcatPermissioner concat permissioner
 func ConcatPermissioner(ps ...Permissioner) Permissioner {
 	var newPS []Permissioner
@@ -18,6 +20,7 @@ func ConcatPermissioner(ps ...Permissioner) Permissioner {
 
 type permissioners []Permissioner
 
+// 检查是否具有权限
 // HasPermission check has permission for permissioners or not
 func (ps permissioners) HasPermission(mode PermissionMode, roles ...interface{}) bool {
 	for _, p := range ps {
